8cee800d759b13cf6829e21cb5e2bd1866070a7a
yanweiser <47057387+yanweiser@users.noreply.github.com>
2021-01-06 14:00:46 +0100

    making it less complicated and more hardcoded

--- mines-pygame/highscore.py
+++ mines-pygame/highscore.py
@@ -5,44 +5,34 @@ import numpy as np
 class Highscore:
     def __init__(self):
         self.hs = []
-
-    def readdata(self, file):
-        f = open(file, "r")
-        return f
-
-    def writedata(self, file):
-        f = open(file,"w")
-        return f
+        if os.path.exists('highscore.txt'):
+            self.file = 'highscore.txt'
+        elif os.path.exists('mines-pygame/highscore.txt'):
+            self.file = 'mines-pygame/highscore.txt'
+        else:
+            file = open('highscore.txt','w+')
+            file.write('0,0,0')
+            file.close()
+            self.file = 'highscore.txt'
 
     def form(self, file):
+        file = open(self.file, 'r')
         dl = file.read()
         x = []
-        args = dl.split(",", 3)
-        print(args)
+        args = dl.split(",")
         for a in args:
-            print(a)
-            a = a.replace(" ", "")
-            a = a.replace("\n", "")
-            a = a.replace(",", "")
-            if a == "":
-                continue
-            a = int(a)
+            a = int(a.strip())
             x.append(a)
-        while len(x) != 3:
-            x.append(0)
         self.hs = x
         file.close()
 
-    def writeHS(self, file):
+    def writeHS(self):
+        file = open(self.file, 'w')
         file.write(self.makeStr())
         file.close()
 
     def makeStr(self):
         s = ""
         for h in self.hs:
-            print(s)
             s = s + str(h) + ","
-        return s
-
-
-
+        return s[:-1]
