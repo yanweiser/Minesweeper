5099567b3c9e8b52969bd4a11eccb5b003ddfd17
yanweiser <47057387+yanweiser@users.noreply.github.com>
2021-01-06 13:50:05 +0100

    now works with reworked Square class

--- mines-pygame/game.py
+++ mines-pygame/game.py
@@ -24,7 +24,7 @@ class Game:
             self.sqrs.append([])
         for i in range(0,(n*GAMESIZE) - (n-1),n):
             for j in range(0,(n*GAMESIZE) - (n-1),n):
-                self.sqrs[i//n].append(Square(i+5,j+5,(200,200,200),self))
+                self.sqrs[i//n].append(Square(i+5,j+5,i//n,j//n,(200,200,200),self))
 
     def finish(self):
         for i in range(0,self.GAMESIZE):
@@ -32,47 +32,57 @@ class Game:
                 if self.field[i][j] == 0 and self.bombs[i][j] == 0:
                     self.sqrs[i][j].set(self)
 
-    def adj(self,ar,x,y):
+    def adj(self,bomb,x,y):
         ret = 0
         for i in range(x-1,x+2):
             for j in range(y-1,y+2):
                 if i in range(0,self.GAMESIZE) and j in range(0,self.GAMESIZE):
-                    if ar:
+                    if bomb:
                         ret += self.bombs[i][j]
                     else:
                         ret += self.field[i][j]
         return ret
 
     def propagate(self,x,y):
-        for j in range(x-1,x+2):
-            for i in range(y-1,y+2):
+        for i in range(x-1,x+2):
+            for j in range(y-1,y+2):
                 if (i,j) == (x,y) or i not in range(0,self.GAMESIZE) or j not in range(0,self.GAMESIZE):
                     continue
                 if not self.field[i][j] and not self.bombs[i][j]:
                     self.sqrs[i][j].set(self)
-                    if not self.adj(True,i,j):
-                        self.propagate(j,i)
+                    if not self.adjBombs[i][j]:
+                        self.propagate(i,j)
         return
+    
+    
+    def near(self,t1,t2):
+        if t1 in ((t2[0],t2[1]),(t2[0],t2[1]+1),(t2[0],t2[1]-1),(t2[0]+1,t2[1]),(t2[0]-1,t2[1]),(t2[0]+1,t2[1]+1),(t2[0]+1,t2[1]-1),(t2[0]-1,t2[1]+1),(t2[0]-1,t2[1]-1)):
+            return True
+        return False
 
     def generate(self,sqr):
-        x = sqr.getX(self)
-        y = sqr.getY(self)
+        x = sqr.col
+        y = sqr.row
         while self.bombs.sum() < self.BOMBCOUNT:
             r = rd.randint(0,(self.GAMESIZE*self.GAMESIZE) - 1 )
             e = 0
             while r > self.GAMESIZE - 1:
                 e+=1
                 r-=self.GAMESIZE
+            if self.bombs[e][r] or self.near((e,r),(x,y)):
+                continue
             self.bombs[e][r] = 1
-            if self.adj(True, x,y) or self.adj(True,e,r) > 2:
-                self.bombs[e][r] = 0
+            for i in range(0,self.GAMESIZE):
+                for j in range(0,self.GAMESIZE):
+                    if self.adj(True,i,j) > 2:
+                        self.bombs[e][r] = 0
                 
         for i in range(0,self.GAMESIZE):
             for j in range(0,self.GAMESIZE):
                 self.adjBombs[i][j] = self.adj(True,i,j)
 
         self.sqrs[x][y].set(self)
-        self.propagate(y,x)
+        self.propagate(x,y)
 
     def won(self):
         if self.field.sum() + self.bombs.sum() == self.GAMESIZE**2:
